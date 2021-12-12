b6c25a751d4b82d163887188d9d1681c41b2d5cb
juo <hwgiwh271@gmail.com>
2021-12-12 22:57:39 +0900

    add: create PageComponent
    1. PageComponent
    
    -

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,15 @@
 console.log(import.meta.webpack);
 import "../style/css/style.css";
 
+const err = (msg: string) => {
+  throw msg;
+};
+const el = <T extends keyof HTMLElementTagNameMap>(v: T) =>
+  document.createElement(v);
+
+interface Component {
+  attachTo(parent: HTMLElement, position?: InsertPosition): void;
+}
 if (module.hot) {
   console.log("핫모듈!");
   // module.hot.accept("./result", async () => {
@@ -10,3 +19,58 @@ if (module.hot) {
   //   resultEl.innerHTML = await result.render();
   // });
 }
+
+const BaseComponent = class<T extends HTMLElement> implements Component {
+  protected readonly el: T;
+  constructor(html: string) {
+    const template = el("template");
+    template.innerHTML = html;
+    this.el = template.content.firstElementChild as T;
+  }
+  attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
+    parent.insertAdjacentElement(position, this.el);
+  }
+};
+
+const PageComponent = class extends BaseComponent<HTMLElement> {
+  constructor() {
+    super(`<div class="page-container"></div>`);
+  }
+  addChild(child: Component) {
+    child.attachTo(this.el);
+  }
+};
+const PageLeftComponent = class extends BaseComponent<HTMLElement> {
+  constructor() {
+    super(`<div class="page-left"></div>`);
+  }
+};
+const PageRightComponent = class extends BaseComponent<HTMLElement> {
+  constructor() {
+    super(`<div class="page-right"></div>`);
+  }
+};
+
+const Renderer = class {
+  constructor() {}
+  render() {
+    this._render();
+  }
+  _render() {
+    err("must be overrided!");
+  }
+};
+
+const DomRenderer = class extends Renderer {
+  constructor(private parent: HTMLElement) {
+    super();
+    const page = new PageComponent();
+    page.attachTo(this.parent);
+    [new PageLeftComponent(), new PageRightComponent()].forEach((v) =>
+      page.addChild(v)
+    );
+  }
+  override _render() {}
+};
+
+new DomRenderer(document.querySelector(".main") as HTMLElement);
